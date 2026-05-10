ca292e194f4bf9fe3d0f1974017e428eedc65735
Roy@Souharda <b123134@iiit-bh.ac.in>
2026-05-10 18:39:57 +0530

    Commit-XCI

---
+++ cses/1652.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+#define MOD 1000000007
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+static inline void fast_io() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+static inline unsigned long long splitmix64(unsigned long long x) {
+    x += 0x9e3779b97f4a7c15ULL;
+    x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9ULL;
+    x = (x ^ (x >> 27)) * 0x94d049bb133111ebULL;
+    return x ^ (x >> 31);
+}
+
+static mt19937_64 rng((unsigned)splitmix64(chrono::high_resolution_clock::now().time_since_epoch().count()));
+
+struct chash {
+    size_t operator()(unsigned long long x) const {
+        static const unsigned long long FIXED_RANDOM = splitmix64(chrono::high_resolution_clock::now().time_since_epoch().count());
+        return (size_t)splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+#ifndef ONLINE_JUDGE
+  #define dbg(...) do{ fprintf(stderr, "DBG (%s:%d) = ", __FILE__, __LINE__); fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n"); } while(0)
+  #define debug_var(x) cerr << #x << " = " << (x) << "\n";
+#else
+  #define dbg(...) (void)0
+  #define debug_var(x) (void)0
+#endif
+
+template<class T> inline T aabs(T x){ return x < 0 ? -x : x; }
+template<class T> inline T ceil_div(T a, T b){ return (a + b - 1) / b; }
+template<class T> inline void chmin(T &a, T b){ if(b < a) a = b; }
+template<class T> inline void chmax(T &a, T b){ if(b > a) a = b; }
+
+void solver() {
+    int n, q;
+    cin >> n >> q;
+    vector<string> forest(n);
+    for (string &it : forest)
+        cin >> it;
+    int m = forest[0].size();
+    vector<vi> prefixes(n, vi(m, 0));
+    for (int i = 0; i < n; i++) {
+        prefixes[i][0] = forest[i][0] == '*';
+        for (int j = 1; j < m; j++) 
+            prefixes[i][j] += prefixes[i][j - 1] + (forest[i][j] == '*');
+    }
+    for (int i = 1; i < n; i++) 
+        for (int j = 0; j < m; j++) 
+            prefixes[i][j] += prefixes[i - 1][j];
+    auto query = [&] (int r1, int c1, int r2, int c2) -> int {
+        return prefixes[r2][c2] - ((r1 > 0) ? prefixes[r1 - 1][c2] : 0) - ((c1 > 0) ? prefixes[r2][c1 - 1] : 0) + ((r1 > 0 && c1 > 0) ? prefixes[r1 - 1][c1 - 1] : 0);
+    };
+    int r1, c1, r2, c2;
+    while (q--) {
+        cin >> r1 >> c1 >> r2 >> c2;
+        cout << query(--r1, --c1, --r2, --c2) << endl;
+    }
+}
+
+int main() {
+    fast_io();
+    #ifdef LOCAL
+        freopen("a.in", "r", stdin);
+    #endif
+
+    solver();
+
+    return 0;
+}
